426aebf9d6bd7b689df90e5bed8a8028ccd53883
jacky6016 <chlin6016@gmail.com>
2016-09-28 11:54:05 +0800

    Fix typo

--- math-toolkit.h
+++ math-toolkit.h
@@ -35,7 +35,7 @@ void subtract_vector(const double *a, const double *b, double *out)
 {
 	out[0] = a[0] - b[0];
 	out[1] = a[1] - b[1];
-+	out[2] = a[2] - b[2];
+	out[2] = a[2] - b[2];
 }
 
 static inline
